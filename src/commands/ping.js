d5942c460a32ab4ad6ed4000d1e03b9da8ae325c
curse <58047023+cursecodes@users.noreply.github.com>
2023-01-08 13:45:48 -0700

    Better ping

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -5,6 +5,18 @@ export default {
     .setName("ping")
     .setDescription("Replies with Pong!"),
   async execute(interaction) {
-    await interaction.reply("Pong!");
+    const sent = await interaction.reply({
+      content: "Pinging...",
+      fetchReply: true,
+    });
+    await interaction.editReply(
+      `:ping_pong: Pong!\n:stopwatch: Uptime: \`${Math.round(
+        interaction.client.uptime / 60000
+      )}\` minutes\n:sparkling_heart: Websocket heartbeat: \`${
+        interaction.client.ws.ping
+      }ms\`.\n:round_pushpin: Rountrip Latency: \`${
+        sent.createdTimestamp - interaction.createdTimestamp
+      }\`ms`
+    );
   },
 };
