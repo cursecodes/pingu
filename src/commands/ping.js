d9cc95b8a04516b82a73e98f44e398d2251c4d04
curse <58047023+cursecodes@users.noreply.github.com>
2023-01-08 14:26:42 -0700

    Ping embed

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -9,14 +9,72 @@ export default {
       content: "Pinging...",
       fetchReply: true,
     });
-    await interaction.editReply(
-      `:ping_pong: Pong!\n:stopwatch: Uptime: \`${Math.round(
-        interaction.client.uptime / 60000
-      )}\` minutes\n:sparkling_heart: Websocket heartbeat: \`${
-        interaction.client.ws.ping
-      }ms\`.\n:round_pushpin: Rountrip Latency: \`${
-        sent.createdTimestamp - interaction.createdTimestamp
-      }\`ms`
-    );
+
+    const pingEmbed = {
+      title: ":ping_pong: Pong!",
+      color: 3553598,
+      fields: [
+        {
+          name: ":stopwatch: Uptime",
+          value: `\`${Math.round(interaction.client.uptime / 60000)} minutes\``,
+          inline: true,
+        },
+        {
+          name: ":sparkling_heart: WS heartbeat",
+          value: `\`${interaction.client.ws.ping}ms\``,
+          inline: true,
+        },
+        {
+          name: ":round_pushpin: Roundtrip Latency",
+          value: `\`${
+            sent.createdTimestamp - interaction.createdTimestamp
+          }ms\``,
+          inline: true,
+        },
+      ],
+      footer: {
+        text: "Pingu",
+        icon_url:
+          "https://cdn.e-z.host/e-zimagehosting/07839b7c-ff5c-4dc5-952a-f78bef48d49a/car5lqcn.png",
+      },
+      timestamp: "2023-01-08T21:21:00.000Z",
+    };
+
+    // const embed = new EmbedBuilder()
+    //   .setImage(
+    //     "https://cdn.e-z.host/e-zimagehosting/07839b7c-ff5c-4dc5-952a-f78bef48d49a/car5lqcn.png"
+    //   )
+    //   .setColor(0x36393e)
+    //   .setAuthor({
+    //     name: "Pingu",
+    //     iconUrl:
+    //       "https://cdn.e-z.host/e-zimagehosting/07839b7c-ff5c-4dc5-952a-f78bef48d49a/car5lqcn.png",
+    //   })
+    //   .setTitle(":ping_pong: Pong!")
+    //   .addFields(
+    //     {
+    //       name: ":stopwatch: Uptime",
+    //       value: `\`${Math.round(interaction.client.uptime / 60000)} minutes\``,
+    //     },
+    //     {
+    //       name: ":sparkling_heart: Websocket heartbeat",
+    //       value: `\`${interaction.client.ws.ping}ms\``,
+    //     },
+    //     {
+    //       name: ":round_pushpin: Rountrip Latency",
+    //       value: `\`${
+    //         sent.createdTimestamp - interaction.createdTimestamp
+    //       }ms\``,
+    //     }
+    //   )
+    //   .setFooter({
+    //     text: "Pingu",
+    //     iconUrl:
+    //       "https://cdn.e-z.host/e-zimagehosting/07839b7c-ff5c-4dc5-952a-f78bef48d49a/car5lqcn.png",
+    //   })
+    //   .setTimestamp();
+
+    await interaction.editReply("Pong! Data below.");
+    await interaction.followUp({ embeds: [pingEmbed] });
   },
 };
